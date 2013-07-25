dc5251985c34c9ffa1047b41e9a323de167adb0e
Young Hahn <young@developmentseed.org>
2013-07-25 14:51:31 -0400

    Remove v2 references from history tests.

--- test/tm.test.js
+++ test/tm.test.js
@@ -28,59 +28,49 @@ describe('tm', function() {
 
     it('history', function() {
         assert.deepEqual({style:[], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history(),
             'Inits with defaults');
         assert.deepEqual({style:[], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history('insufficient args'),
             'Does not attempt set without enough args');
         assert.throws(function() { tm.history('badtype', 'foo') }, /requires valid type/,
             'Throws error on bad type');
         assert.deepEqual({style:['foo'], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history('style', 'foo'),
             'Sets style');
         assert.deepEqual({style:['foo'], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history(),
             'Confirm set');
         assert.deepEqual({style:['foo'], source:[
             'bar',
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history('source', 'bar'),
             'Sets source');
         assert.deepEqual({style:['foo'], source:[
             'bar',
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history(),
             'Confirm set');
         assert.deepEqual({style:['foo'], source:[
             'bar',
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history('source', 'bar'),
             'Ignores duplicates');
         assert.deepEqual({style:['foo'], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history('source', 'bar', true),
             'Invalidates source');
         assert.deepEqual({style:['foo'], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
         ]}, tm.history(),
             'Confirm invalidation');
         assert.deepEqual({style:['foo'], source:[
-            'mapbox:///mapbox.mapbox-streets-v2',
             'mapbox:///mapbox.mapbox-streets-v3'
-        ]}, tm.history('source', 'mapbox:///mapbox.mapbox-streets-v2', true),
+        ]}, tm.history('source', 'mapbox:///mapbox.mapbox-streets-v3', true),
             'Cannot invalidate default source');
     });
 });
