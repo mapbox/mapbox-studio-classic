10180038a56763fb2b5fb0ce6bfff462e42e518f
tristen <tristen.brown@gmail.com>
2014-03-10 15:12:02 -0400

    Drop in some stuff

---
+++ test/browser/test.events.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Editor', function() {
+    it('should trigger tab creation');
+    it('should delete a tab');
+    it('should reject file formats in the filename');
+    it('should trigger layers pane');
+    it('should expand to fullscreen');
+    it('should open settings pane');
+    it('should open layers pane');
+});
+
+describe('Layers', function() {
+    it('should open layer descriptions');
+    it('should open the data pane');
+});
+
+describe('Documentation', function() {
+    it('should tab through help topics')
+});
