631e89ea91f62d339ab218251e90614e1c949fd4
Eden Halperin <eden@mapbox.com>
2014-03-13 14:49:53 -0700

    update travis

--- test/fixtures-oauth/mapbox.js
+++ test/fixtures-oauth/mapbox.js
@@ -1,17 +1,12 @@
-var tm = require('../../lib/tm');
-
 var express = require('express');
 var app = express();
 app.use(app.router);
 
 app.get('/api/User/test?access_token=testaccesstoken', function(req, res, next) {
-	console.log('test user');
-	return {statusCode:200};
+	res.send({statusCode:200});
 });
-
 app.get('/api/Map/test.tm2-basemap?access_token=testaccesstoken', function(req, res, next) {
-	console.log('test map');
-	return {statusCode:200};
+	res.send({statusCode:200});
 });
 
 app.listen(3001);
