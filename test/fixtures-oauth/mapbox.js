b9a209e0de11e162b9a42a00fbc786a229f1c1b6
Young Hahn <young@developmentseed.org>
2014-03-13 19:04:35 -0400

    Don't include querystring in routes.

--- test/fixtures-oauth/mapbox.js
+++ test/fixtures-oauth/mapbox.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 app.use(app.router);
 
-app.get('/api/User/test?access_token=testaccesstoken', function(req, res, next) {
+app.get('/api/User/test', function(req, res, next) {
 	res.send({statusCode:200});
 });
-app.get('/api/Map/test.tm2-basemap?access_token=testaccesstoken', function(req, res, next) {
+app.get('/api/Map/test.tm2-basemap', function(req, res, next) {
 	res.send({statusCode:200});
 });
 
