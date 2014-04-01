d12ee8456db29ff260d1a7e4f3af7a576fd2ccf5
Aaron Lidman <aaronlidman@gmail.com>
2014-04-01 17:30:30 -0400

    Prevent tmp package crashing
    Too naive?

--- index.js
+++ index.js
@@ -310,24 +310,21 @@ app.get('/:style(style).tm2z', function(req, res, next) {
 
 app.get('/upload', auth, function(req, res, next) {
     if (typeof tm.db._docs.user.plan.tm2z == 'undefined' || !tm.db._docs.user.plan.tm2z) {
-        return res.send('You are not allowed access to tm2z uploads, yet.', 404);
+        return res.send('You are not allowed access to tm2z uploads, yet.', 403);
     }
 
     var uri = url.parse(req.query.styleid);
     var pckage = uri.pathname + '/package-' + +new Date + '.tm2z';
     style.toPackage(req.query.styleid, pckage, function(err) {
-        if (err) next(err);
+        if (err) return res.send(err.toString(), 400);
         upload({
             file: pckage,
             account: tm.db._docs.oauth.account,
             accesstoken: tm.db._docs.oauth.accesstoken,
             mapid: tm.db._docs.oauth.account + '.' + +new Date
         }, function(err, task) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('good day sir');
-            }
+            if (err) return res.send(err.toString(), 400);
+            console.log('good day sir');
             res.end();
         });
     });
