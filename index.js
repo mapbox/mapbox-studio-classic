2a2e69f6feb1f284acf3c1f5c7d566a1b2450eda
samanpwbb <samanpwbb@gmail.com>
2014-04-18 15:02:27 -0400

    request maps when opening addmapbox modal

--- index.js
+++ index.js
@@ -509,5 +509,10 @@ app.get('/geocode', auth, function(req, res, next) {
     res.redirect(query.replace('{query}', req.query.search));
 });
 
+app.get('/listmapbox', auth, function(req, res, next) {
+    var query = 'https://api.mapbox.com/api/Map?account='+tm.db._docs.user.id+'?private=1?list=1';
+    res.redirect(query);
+});
+
 app.listen(config.port);
 console.log('TM2 @ http://localhost:'+config.port+'/');
