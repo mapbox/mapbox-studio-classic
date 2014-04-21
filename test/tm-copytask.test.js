c0e20a30d15390a492e9e587f116bf0ae46007ca
Ryan Clark <ryan.clark.j@gmail.com>
2014-04-21 10:44:27 -0700

    fix test to adjusted size, likely result of tolerance-32 work

--- test/tm-copytask.test.js
+++ test/tm-copytask.test.js
@@ -40,7 +40,7 @@ describe('copytask', function() {
                         basename: 'export.mbtiles',
                         id: 'export',
                         format: 'pbf',
-                        filesize: 45056,
+                        filesize: 44032,
                         attribution: '&copy; John Doe 2013.',
                         center: [ 0, 0, 3 ],
                         bounds: [ -180, -85.0511, 180, 85.0511 ],
