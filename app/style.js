e5d37e796313d58428f964251f8a449818e6a2df
Camille Teicheira <cteicheira@gmail.com>
2014-05-14 15:09:47 -0400

    remove scale slider from ui

--- app/style.js
+++ app/style.js
@@ -526,13 +526,14 @@ Editor.prototype.recache = function(ev) {
 };
 Editor.prototype.save = function(ev, options) {
   var editor = this;
+
   // Set map in loading state.
   $('#full').addClass('loading');
 
   var attr = {};
   // Grab settings form values.
   _($('#settings').serializeArray()).reduce(function(memo, field) {
-    if (field.name === 'minzoom' || field.name === 'maxzoom' || field.name === 'scale') {
+    if (field.name === 'minzoom' || field.name === 'maxzoom') {
       memo[field.name] = parseInt(field.value,10);
     } else if (field.name === 'baselayer') {
       if (field.value) {
