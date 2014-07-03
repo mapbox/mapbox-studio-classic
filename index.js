ae7cde6545d5391a6cda786d65f5a9d21fc7f071
Young Hahn <young@mapbox.com>
2014-07-03 03:19:48 -0400

    Tweaks to error handling.

--- index.js
+++ index.js
@@ -335,16 +335,13 @@ app.get('/style.tm2z', middleware.style, function(req, res, next) {
 });
 
 app.get('/upload', middleware.auth, function(req, res, next) {
-    if (style.tmpid(req.query.styleid))
-        return next(new Error('Style must be saved first'));
-
     style.upload({
         id: req.query.styleid,
         oauth: tm.db.get('oauth'),
         cache: tm.config().cache
-    }, function(err){
+    }, function(err, info) {
         if (err) next(err);
-        res.end();
+        res.send(info);
     });
 });
 
