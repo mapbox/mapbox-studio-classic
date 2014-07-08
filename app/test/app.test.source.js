07c5fd34fc4f08ead173e6adef147761e80da380
Young Hahn <young@mapbox.com>
2014-07-08 10:30:40 -0400

    De-indent the things.

--- app/test/app.test.source.js
+++ app/test/app.test.source.js
@@ -20,69 +20,69 @@ $(document).ajaxComplete(function() {
     setTimeout(function() { callback(); }, 100);
 });
 
-    tape('Setting maxzoom: sets maxzoom to higher value than 6 (tests logic preference for higher maxzoom...see #addlayer-shape test)', function(t) {
-        var maxzoomTarget = $('.js-settings-form #maxzoom');
-        maxzoomTarget.val(12);
-        $('.js-save').submit();
-        var maxzoom = maxzoomTarget.val();
-        t.equal(maxzoom, '12');
-        t.end();
-    });
+tape('Setting maxzoom: sets maxzoom to higher value than 6 (tests logic preference for higher maxzoom...see #addlayer-shape test)', function(t) {
+    var maxzoomTarget = $('.js-settings-form #maxzoom');
+    maxzoomTarget.val(12);
+    $('.js-save').submit();
+    var maxzoom = maxzoomTarget.val();
+    t.equal(maxzoom, '12');
+    t.end();
+});
 
-    tape('#addlayer-shape: adds new shapefile and checks input values', function(t) {
-        //Browse for file and add new shape layer
-        $('.js-addlayer').click();
-        $('.js-browsefile').click();
-        var cwd = $('div.cwd').text();
-        //This RegEx can probably be cleaned up, but it works for now
-        cwd = cwd.replace(/\s*$/,"");
-        var array = cwd.split(/[\s,]+/);
-        var shpFile = array[1] + '/test/fixtures-localsource/10m-900913-bounding-box.shp';
-        $('#browsefile .col8').val(shpFile);
-        $('#browsefile .col4').submit();
-        onajax(function() {
-        	var maxzoomTarget = $('.js-settings-form #maxzoom');
-        	var maxzoom = maxzoomTarget.val();
-        	var projTarget = $('.js-metadata-projection');
-   			var expectedValue = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over';
-			t.equal(expectedValue, projTarget.val());
-        	t.equal(maxzoom, '12');
-        	t.end();
-        });
-    });
-
-    tape('sets maxzoom', function(t) {
-        var maxzoomTarget = $('.js-settings-form #maxzoom');
-        maxzoomTarget.val(6);
-        $('.js-save').submit();
-        var maxzoom = maxzoomTarget.val();
-        t.equal(maxzoom, '6');
-        t.end();
-    });
-
-	tape('tests the projection input field is populated with the expected projection', function(t) {
-		var projTarget = $('.js-metadata-projection');
+tape('#addlayer-shape: adds new shapefile and checks input values', function(t) {
+    //Browse for file and add new shape layer
+    $('.js-addlayer').click();
+    $('.js-browsefile').click();
+    var cwd = $('div.cwd').text();
+    //This RegEx can probably be cleaned up, but it works for now
+    cwd = cwd.replace(/\s*$/,"");
+    var array = cwd.split(/[\s,]+/);
+    var shpFile = array[1] + '/test/fixtures-localsource/10m-900913-bounding-box.shp';
+    $('#browsefile .col8').val(shpFile);
+    $('#browsefile .col4').submit();
+    onajax(function() {
+    	var maxzoomTarget = $('.js-settings-form #maxzoom');
+    	var maxzoom = maxzoomTarget.val();
+    	var projTarget = $('.js-metadata-projection');
 		var expectedValue = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over';
-		t.deepEqual(expectedValue, projTarget.val());
-        t.end();
-	});
-
-    tape('#updatename-shape: updates the layer name and checks that input values and new layer modal are set', function(t) {
-        //Set description of old layer
-        $('.js-layer #10m-900913-bounding-box').click();
-        $('#10m-900913-bounding-box-buffer-size').val('24');
-        var expectedBuffer = $('#10m-900913-bounding-box-buffer-size').val();
-
-        //Update layer name
-        $('#updatename-10m-900913-bounding-box').click();
-        $('#newLayername').val('hey');
-        $('#updatename').submit();
-
-        var currentUrl = window.location.toString();
-        var newBufferTarget = $('#hey-buffer-size-val');
-
-        t.equal(currentUrl.slice(-10),'layers-hey');
-        t.equal(expectedBuffer, newBufferTarget.text());
-        t.end();
+		t.equal(expectedValue, projTarget.val());
+    	t.equal(maxzoom, '12');
+    	t.end();
     });
+});
+
+tape('sets maxzoom', function(t) {
+    var maxzoomTarget = $('.js-settings-form #maxzoom');
+    maxzoomTarget.val(6);
+    $('.js-save').submit();
+    var maxzoom = maxzoomTarget.val();
+    t.equal(maxzoom, '6');
+    t.end();
+});
+
+tape('tests the projection input field is populated with the expected projection', function(t) {
+	var projTarget = $('.js-metadata-projection');
+	var expectedValue = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over';
+	t.deepEqual(expectedValue, projTarget.val());
+    t.end();
+});
+
+tape('#updatename-shape: updates the layer name and checks that input values and new layer modal are set', function(t) {
+    //Set description of old layer
+    $('.js-layer #10m-900913-bounding-box').click();
+    $('#10m-900913-bounding-box-buffer-size').val('24');
+    var expectedBuffer = $('#10m-900913-bounding-box-buffer-size').val();
+
+    //Update layer name
+    $('#updatename-10m-900913-bounding-box').click();
+    $('#newLayername').val('hey');
+    $('#updatename').submit();
+
+    var currentUrl = window.location.toString();
+    var newBufferTarget = $('#hey-buffer-size-val');
+
+    t.equal(currentUrl.slice(-10),'layers-hey');
+    t.equal(expectedBuffer, newBufferTarget.text());
+    t.end();
+});
 
--- app/test/app.test.style.js
+++ app/test/app.test.style.js
@@ -43,143 +43,143 @@ tape('saves a project', function() {
 });
 */
 
-    tape('.js-history browses sources', function(t) {
-        $('.js-history .js-browsesource').click();
-        t.ok(hasModal('#browsesource'));
+tape('.js-history browses sources', function(t) {
+    $('.js-history .js-browsesource').click();
+    t.ok(hasModal('#browsesource'));
+    t.end();
+});
+
+tape('.js-history browses styles', function(t) {
+    $('.js-history .js-browsestyle').click();
+    t.ok(hasModal('#browsestyle'));
+    t.end();
+});
+
+tape('.js-history removes history style', function(t) {
+    var count = $('#history-style .project').size();
+    $('.js-history .js-ref-delete:eq(0)').click();
+    onajax(function() {
+        t.equal(count - 1, $('#history-style .project').size());
         t.end();
     });
+});
 
-    tape('.js-history browses styles', function(t) {
-        $('.js-history .js-browsestyle').click();
-        t.ok(hasModal('#browsestyle'));
-        t.end();
-    });
+tape('#style-ui creates a new tab', function(t) {
+    $('.js-addtab:eq(0)').click();
+    t.ok(hasModal('form#addtab'));
 
-    tape('.js-history removes history style', function(t) {
-        var count = $('#history-style .project').size();
-        $('.js-history .js-ref-delete:eq(0)').click();
+    $('#addtab-filename').val('foo');
+    $('#addtab').submit();
+
+    // Submit removes modal.
+    t.equal(0, $('#addtab-filename').size());
+
+    // Automatically adds .mss extension.
+    t.equal('foo.mss', $('#tabs .js-tab:last').attr('rel'));
+
+    t.end();
+});
+
+tape('#style-ui clicks set tabs as active', function(t) {
+    $('#tabs .js-tab:eq(1)').click();
+    t.ok($('#tabs .js-tab:eq(1)').hasClass('active'));
+    t.ok(!$('#tabs .js-tab:eq(0)').is('.active'));
+
+    $('#tabs .js-tab:eq(0)').click();
+    t.ok($('#tabs .js-tab:eq(0)').hasClass('active'));
+    t.ok(!$('#tabs .js-tab:eq(1)').is('.active'));
+
+    t.end();
+});
+
+tape('#style-ui keys set tabs as active', function(t) {
+    var e;
+    // ctrl+alt+1
+    e = $.Event('keydown');
+    e.which = ('1').charCodeAt(0);
+    e.altKey = true;
+    e.ctrlKey = true;
+    $('body').trigger(e);
+    t.ok($('#tabs .js-tab:eq(0)').hasClass('active'));
+    t.ok(!$('#tabs .js-tab:eq(1)').is('.active'));
+
+    // ctrl+alt+2
+    e = $.Event('keydown');
+    e.which = ('2').charCodeAt(0);
+    e.altKey = true;
+    e.ctrlKey = true;
+    $('body').trigger(e);
+    t.ok($('#tabs .js-tab:eq(1)').hasClass('active'));
+    t.ok(!$('#tabs .js-tab:eq(0)').is('.active'));
+
+    t.end();
+});
+
+tape('#style-ui deletes a tab', function(t) {
+    var count = $('#tabs .js-tab').size();
+    $('#tabs .js-deltab:eq(0)').click();
+    t.equal(count - 1, $('#tabs .js-tab').size());
+    t.end();
+});
+
+tape('#style-ui prevents duplicate extensions in filename', function(t) {
+    $('.js-addtab:eq(0)').click();
+    t.ok(hasModal('#addtab'));
+
+    $('#addtab-filename').val('bar.mss');
+    $('#addtab').submit();
+
+    // Submit removes modal.
+    t.ok(!hasModal('#addtab'));
+
+    // Prevents duplicate .mss extension.
+    t.equal('bar.mss', $('#tabs .js-tab:last').attr('rel'));
+
+    t.end();
+});
+
+tape('#style-ui requires unique stylesheet name', function(t) {
+    $('.js-addtab:eq(0)').click();
+    t.ok(hasModal('form#addtab'));
+
+    $('#addtab-filename').val('baz');
+    $('#addtab').submit();
+
+    $('.js-addtab:eq(0)').click();
+    t.ok(hasModal('form#addtab'));
+
+    $('#addtab-filename').val('baz');
+    $('#addtab').submit();
+
+    t.ok(hasModal('#error'));
+    t.equal('Tab name must be different than existing tab "baz"', $('#error > pre').text());
+
+    t.end();
+});
+
+tape('.js-layers opens layer description', function(t) {
+    $('.js-layers .js-tab:eq(0)').click();
+    t.ok($('.js-layers .js-tab:eq(0)').hasClass('active'));
+    t.end();
+});
+
+tape('.js-layers shows sources modal', function(t) {
+    $('.js-layers .js-modalsources:eq(0)').click();
+    onajax(function() {
+        t.ok(hasModal('#modalsources'));
+        $('#modalsources-remote .js-adddata:eq(0)').click();
         onajax(function() {
-            t.equal(count - 1, $('#history-style .project').size());
+            t.ok(!hasModal('#modalsources'));
             t.end();
         });
     });
+});
 
-    tape('#style-ui creates a new tab', function(t) {
-        $('.js-addtab:eq(0)').click();
-        t.ok(hasModal('form#addtab'));
-
-        $('#addtab-filename').val('foo');
-        $('#addtab').submit();
-
-        // Submit removes modal.
-        t.equal(0, $('#addtab-filename').size());
-
-        // Automatically adds .mss extension.
-        t.equal('foo.mss', $('#tabs .js-tab:last').attr('rel'));
-
-        t.end();
-    });
-
-    tape('#style-ui clicks set tabs as active', function(t) {
-        $('#tabs .js-tab:eq(1)').click();
-        t.ok($('#tabs .js-tab:eq(1)').hasClass('active'));
-        t.ok(!$('#tabs .js-tab:eq(0)').is('.active'));
-
-        $('#tabs .js-tab:eq(0)').click();
-        t.ok($('#tabs .js-tab:eq(0)').hasClass('active'));
-        t.ok(!$('#tabs .js-tab:eq(1)').is('.active'));
-
-        t.end();
-    });
-
-    tape('#style-ui keys set tabs as active', function(t) {
-        var e;
-        // ctrl+alt+1
-        e = $.Event('keydown');
-        e.which = ('1').charCodeAt(0);
-        e.altKey = true;
-        e.ctrlKey = true;
-        $('body').trigger(e);
-        t.ok($('#tabs .js-tab:eq(0)').hasClass('active'));
-        t.ok(!$('#tabs .js-tab:eq(1)').is('.active'));
-
-        // ctrl+alt+2
-        e = $.Event('keydown');
-        e.which = ('2').charCodeAt(0);
-        e.altKey = true;
-        e.ctrlKey = true;
-        $('body').trigger(e);
-        t.ok($('#tabs .js-tab:eq(1)').hasClass('active'));
-        t.ok(!$('#tabs .js-tab:eq(0)').is('.active'));
-
-        t.end();
-    });
-
-    tape('#style-ui deletes a tab', function(t) {
-        var count = $('#tabs .js-tab').size();
-        $('#tabs .js-deltab:eq(0)').click();
-        t.equal(count - 1, $('#tabs .js-tab').size());
-        t.end();
-    });
-
-    tape('#style-ui prevents duplicate extensions in filename', function(t) {
-        $('.js-addtab:eq(0)').click();
-        t.ok(hasModal('#addtab'));
-
-        $('#addtab-filename').val('bar.mss');
-        $('#addtab').submit();
-
-        // Submit removes modal.
-        t.ok(!hasModal('#addtab'));
-
-        // Prevents duplicate .mss extension.
-        t.equal('bar.mss', $('#tabs .js-tab:last').attr('rel'));
-
-        t.end();
-    });
-
-    tape('#style-ui requires unique stylesheet name', function(t) {
-        $('.js-addtab:eq(0)').click();
-        t.ok(hasModal('form#addtab'));
-
-        $('#addtab-filename').val('baz');
-        $('#addtab').submit();
-
-        $('.js-addtab:eq(0)').click();
-        t.ok(hasModal('form#addtab'));
-
-        $('#addtab-filename').val('baz');
-        $('#addtab').submit();
-
-        t.ok(hasModal('#error'));
-        t.equal('Tab name must be different than existing tab "baz"', $('#error > pre').text());
-
-        t.end();
-    });
-
-    tape('.js-layers opens layer description', function(t) {
-        $('.js-layers .js-tab:eq(0)').click();
-        t.ok($('.js-layers .js-tab:eq(0)').hasClass('active'));
-        t.end();
-    });
-
-    tape('.js-layers shows sources modal', function(t) {
-        $('.js-layers .js-modalsources:eq(0)').click();
-        onajax(function() {
-            t.ok(hasModal('#modalsources'));
-            $('#modalsources-remote .js-adddata:eq(0)').click();
-            onajax(function() {
-                t.ok(!hasModal('#modalsources'));
-                t.end();
-            });
-        });
-    });
-
-    tape('#reference tabs through CartoCSS reference', function(t) {
-        $('#reference .js-tab:last').click();
-        var target = $('#' + $('#reference .js-tab:last').attr('href').split('#').pop());
-        t.ok($('#reference .js-tab:last').hasClass('active'));
-        t.ok(target.hasClass('active'));
-        t.end();
-    });
+tape('#reference tabs through CartoCSS reference', function(t) {
+    $('#reference .js-tab:last').click();
+    var target = $('#' + $('#reference .js-tab:last').attr('href').split('#').pop());
+    t.ok($('#reference .js-tab:last').hasClass('active'));
+    t.ok(target.hasClass('active'));
+    t.end();
+});
 
