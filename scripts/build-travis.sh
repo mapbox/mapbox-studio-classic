f113bcbd3bbc22066a0005bf5a094798f46bcd68
Young Hahn <young@mapbox.com>
2014-08-02 15:24:01 -0400

    Build script adjustments. [publish atom-child]

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -4,12 +4,12 @@ set -e -u
 set -o pipefail
 
 PLATFORM=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
-COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
-GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\-\.]+\]' | grep -oE '[a-z0-9\-\.]+' | tail -n1)
+COMMIT_MESSAGE=$(git show -s --format=%B $1 | tr -d '\n')
+GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\.\-]+\]' | grep -oE '[a-z0-9\-\.]+' | tail -n1)
 
 if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
     echo "Publishing $GITSHA"
-    sudo apt-get install -qy curl unzip nsis pip
+    sudo apt-get install -y curl unzip nsis pip
     sudo pip install awscli
     ./scripts/build-atom.sh "$GITSHA" linux
     ./scripts/build-atom.sh "$GITSHA" darwin
