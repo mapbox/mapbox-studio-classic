b732f2035b497e4601a3edcafbe621e1b610cae3
Young Hahn <young@mapbox.com>
2014-08-02 15:30:33 -0400

    More tweaks. [publish atom-child]

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -5,12 +5,12 @@ set -o pipefail
 
 PLATFORM=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
 COMMIT_MESSAGE=$(git show -s --format=%B $1 | tr -d '\n')
-GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\.\-]+\]' | grep -oE '[a-z0-9\-\.]+' | tail -n1)
+GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\.\-]+\]' | grep -oE '[a-z0-9\.\-]+' | tail -n1)
 
 if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
     echo "Publishing $GITSHA"
-    sudo apt-get install -y curl unzip nsis python-pip
-    sudo pip install awscli
+    sudo apt-get install -qqy curl unzip nsis python-pip
+    sudo pip install -q awscli
     ./scripts/build-atom.sh "$GITSHA" linux
     ./scripts/build-atom.sh "$GITSHA" darwin
     ./scripts/build-atom.sh "$GITSHA" win32
