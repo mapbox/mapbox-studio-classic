a6d0efde1bdd208f9704092e75dbd2ce08c03d74
Young Hahn <young@mapbox.com>
2014-08-02 15:14:35 -0400

    Move travis's building to separate script. [publish atom-child]

---
+++ scripts/build-travis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -e -u
+set -o pipefail
+
+PLATFORM=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
+COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\-\.]+\]' | grep -oE '[a-z0-9\-\.]+' | tail -n1)
+
+if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
+    echo "Publishing $GITSHA"
+    sudo apt-get install -qy curl unzip nsis pip
+    sudo pip install awscli
+    ./scripts/build-atom.sh "$GITSHA" linux
+    ./scripts/build-atom.sh "$GITSHA" darwin
+    ./scripts/build-atom.sh "$GITSHA" win32
+fi
