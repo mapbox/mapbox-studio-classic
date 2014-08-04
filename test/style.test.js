751a4656a2cd38b343c96b0a1ceb934b60d14806
Young Hahn <young@mapbox.com>
2014-08-03 21:53:27 -0400

    Represent existing styles as in-memory temporary styles using tmpstyle: protocol.

--- test/style.test.js
+++ test/style.test.js
@@ -178,13 +178,36 @@ test('style.info: fails on bad path', function(t) {
 });
 
 test('style.info: reads style YML', function(t) {
-    style.info('tmstyle://' + defpath, function(err, info) {
+    var tmpid = 'tmstyle://' + defpath;
+    style.info(tmpid, function(err, info) {
         t.ifError(err);
-        t.equal(info.id, 'tmstyle://' + defpath, 'style.info adds id key');
+        t.equal(info.id, tmpid, 'style.info adds id key');
+        t.equal(info._tmp, false, 'style info adds _tmp=false');
 
-        info.id = '[id]';
+        var basepath = tm.parse(tmpid).dirname;
+        info.id = info.id.replace(basepath, '[BASEPATH]');
 
         var filepath = path.join(__dirname,'expected','style-info-default.json');
+        if (UPDATE) {
+            fs.writeFileSync(filepath, JSON.stringify(info, null, 2));
+        }
+        t.deepEqual(info, require(filepath));
+        t.end();
+    });
+});
+
+test('style.info: reads style YML (tmp)', function(t) {
+    var tmpid = 'tmpstyle://' + defpath;
+    style.info(tmpid, function(err, info) {
+        t.ifError(err);
+        t.equal(info.id, tmpid, 'style.info adds id key');
+        t.equal(info._tmp, tmpid, 'style info adds _tmp=id');
+
+        var basepath = tm.parse(tmpid).dirname;
+        info.id = info.id.replace(basepath, '[BASEPATH]');
+        info._tmp = info._tmp.replace(basepath, '[BASEPATH]');
+
+        var filepath = path.join(__dirname,'expected','style-info-default-tmp.json');
         if (UPDATE) {
             fs.writeFileSync(filepath, JSON.stringify(info, null, 2));
         }
