e2a26711b2a2bfed3bd4d787d0560ac25c17df5b
Young Hahn <young@mapbox.com>
2014-08-04 13:57:04 -0400

    Add authority, signing identity certs. [publish atom-child]

--- scripts/build-atom.sh
+++ scripts/build-atom.sh
@@ -101,15 +101,19 @@ elif [ $platform == "darwin" ]; then
     mv $build_dir/Atom.app "$build_dir/Mapbox Studio.app"
 
     # Test getting signing key.
-    aws s3 cp "s3://mapbox/mapbox-studio/keys/Mac Developer ID Application: Mapbox, Inc..pem" signing-key.pem
+    aws s3 cp "s3://mapbox/mapbox-studio/keys/Developer ID Certification Authority.cer" authority.cer
+    aws s3 cp "s3://mapbox/mapbox-studio/keys/Developer ID Application: Mapbox, Inc. (GJZR2MEM28).cer" signing-key.cer
     aws s3 cp "s3://mapbox/mapbox-studio/keys/Mac Developer ID Application: Mapbox, Inc..p12" signing-key.p12
     security create-keychain -p travis signing.keychain \
         && echo "+ signing keychain created"
-    security import signing-key.pem -k ~/Library/Keychains/signing.keychain -T /usr/bin/codesign \
-        && echo "+ signing pem added to keychain"
+    security import authority.cer -k ~/Library/Keychains/signing.keychain -T /usr/bin/codesign \
+        && echo "+ authority cer added to keychain"
+    security import signing-key.cer -k ~/Library/Keychains/signing.keychain -T /usr/bin/codesign \
+        && echo "+ signing cer added to keychain"
     security import signing-key.p12 -k ~/Library/Keychains/signing.keychain -P "" -T /usr/bin/codesign \
         && echo "+ signing key added to keychain"
-    rm signing-key.pem
+    rm authority.cer
+    rm signing-key.cer
     rm signing-key.p12
 
     # Sign .app file.
