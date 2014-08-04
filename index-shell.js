4328e7ec40380e0a460cf9a2a1b8f8da1721cdfa
Camille Teicheira <cteicheira@gmail.com>
2014-08-04 13:03:17 -0700

    first stab at menu, keybindings still in question

--- index-shell.js
+++ index-shell.js
@@ -2,6 +2,8 @@ var atom = require('app');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var BrowserWindow = require('browser-window');
+var Menu = require('menu');
+
 
 var node = path.resolve(path.join(__dirname, 'vendor', 'node'));
 var script = path.resolve(path.join(__dirname, 'index-server.js'));
@@ -55,22 +57,26 @@ function makeWindow() {
         mainWindow = null;
     });
 
-    var protocol = require('protocol');
+    // var protocol = require('protocol');
 
-    if (protocol.isHandledProtocol('https')) {
-        protocol.interceptProtocol('https', function(request) {
-          var url = request.url.substr(7)
-          console.log(url);
-          return;
-        });
-    } else {
-        protocol.registerProtocol('https', function(request) {
-          var url = request.url.substr(7)
-          console.log('URL', url)
-          return;
-        });
-    }
+    // if (protocol.isHandledProtocol('https')) {
+    //     console.log('top', protocol.isHandledProtocol('https'))
+    //     protocol.interceptProtocol('https', function(request) {
+    //       console.log(req)
+    //       var url = request.url.substr(7)
+    //       console.log(url);
+    //       return;
+    //     });
+    // } else {
+    //     console.log('bottom')
+    //     protocol.registerProtocol('https', function(request) {
+    //       var url = request.url.substr(7)
+    //       console.log('URL', url)
+    //       return;
+    //     });
+    // }
 
+    createMenu();
     loadURL();
 }
 
@@ -78,5 +84,177 @@ function loadURL() {
     if (!mainWindow) return;
     if (!serverPort) return;
     mainWindow.loadUrl('http://localhost:'+serverPort);
+}
+
+function createMenu() {
+    var template;
+
+    if (process.platform == 'darwin') {
+    template = [
+      {
+        label: 'Mapbox Studio',
+        submenu: [
+          {
+            label: 'About Mapbox Studio',
+            selector: 'orderFrontStandardAboutPanel:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide Mapbox Studio',
+            accelerator: 'Command+H',
+            selector: 'hide:'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:'
+          },
+          {
+            label: 'Show All',
+            selector: 'unhideAllApplications:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          },
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            selector: 'undo:'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            selector: 'redo:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Cut',
+            accelerator: 'Command+X',
+            selector: 'cut:'
+          },
+          {
+            label: 'Copy',
+            accelerator: 'Command+C',
+            selector: 'copy:'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'Command+V',
+            selector: 'paste:'
+          },
+          {
+            label: 'Select All',
+            accelerator: 'Command+A',
+            selector: 'selectAll:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Save',
+            accelerator: 'Command+S',
+            selector: 'save:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Find',
+            accelerator: 'Command+F',
+            selector: 'find:'
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Enter Fullscreen',
+            click: function() { mainWindow.setFullScreen(true); }
+          },
+          {
+            label: 'Toggle DevTools',
+            accelerator: 'Alt+Command+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          },
+        ]
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'Command+M',
+            selector: 'performMiniaturize:'
+          },
+          {
+            label: 'Close',
+            accelerator: 'Command+W',
+            selector: 'performClose:'
+          },
+        ]
+      },
+    ];
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  } else {
+    template = [
+      {
+        label: 'File',
+        submenu: [
+          {
+            label: 'Open',
+            accelerator: 'Ctrl+O',
+          },
+          {
+            label: 'Close',
+            accelerator: 'Ctrl+W',
+            click: function() { mainWindow.close(); }
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Ctrl+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Enter Fullscreen',
+            click: function() { mainWindow.setFullScreen(true); }
+          },
+          {
+            label: 'Toggle DevTools',
+            accelerator: 'Alt+Ctrl+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          },
+        ]
+      },
+    ];
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
+  }
 }
 
