c910913a7f21b76994faff4a9f7d65340e59ff96
samanpwbb <samanpwbb@gmail.com>
2014-08-12 14:45:49 -0700

    cleanup styles + set values based on map center

--- app/source.js
+++ app/source.js
@@ -494,6 +494,10 @@ window.Source = function(templates, cwd, tm, source, revlayers, examples) {
             map.on('zoomend', function() {
                 $('#zoomedto').attr('class', 'contain z' + (map.getZoom() | 0));
             });
+            $('#map-center').text([this.model.get('center')[1].toFixed(4) + ', ' + this.model.get('center')[0].toFixed(4)]);
+            map.on('move', _.throttle(function(e) {
+                $('#map-center').text(map.getCenter().lat.toFixed(4) + ', ' + map.getCenter().lng.toFixed(4));
+            }, 100));
             map.on('click', inspectFeature({
                 id: this.model.id,
                 type: 'source',
--- app/style.js
+++ app/style.js
@@ -425,8 +425,9 @@ Editor.prototype.refresh = function(ev) {
     map = L.mapbox.map('map');
     map.setView([this.model.get('center')[1], this.model.get('center')[0]], this.model.get('center')[2]);
     map.on('zoomend', function() { $('#zoomedto').attr('class', 'contain z' + (map.getZoom()|0)); });
-    map.on('mousemove', _.throttle(function(e) {
-        $('#map-center').text(e.latlng.lat.toFixed(4) + ', ' + e.latlng.lng.toFixed(4));
+    $('#map-center').text([this.model.get('center')[1].toFixed(4) + ', ' + this.model.get('center')[0].toFixed(4)]);
+    map.on('move', _.throttle(function(e) {
+        $('#map-center').text(map.getCenter().lat.toFixed(4) + ', ' + map.getCenter().lng.toFixed(4));
     }, 100));
     map.on('click', inspectFeature({
       id: this.model.id,
