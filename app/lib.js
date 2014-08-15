e89062691d2da013cea325c089282aee3ec2f5e2
peterqliu <peterqliu@gmail.com>
2014-08-14 17:55:05 -0700

    catchup

--- app/lib.js
+++ app/lib.js
@@ -4,8 +4,8 @@
 //       var url = request.url.substr(7)
 //       console.log('URL', url)
 //     });
-  window.listofbookmarks='';
-
+if (localStorage.listofbookmarks){}
+  else{localStorage.setItem('listofbookmarks','')};
 var tabbedHandler = function(ev) {
   var target = ev.currentTarget.href.split('#').pop();
   var context = target.split('-').slice(0,-1).join('-');
@@ -373,8 +373,8 @@ views.Maputils.prototype.addbookmark = function(ev) {
   field.val('');
   this.appendBookmark(name);
 
-  listofbookmarks={place_name:name,zoom:zoom,center:coords};
-  localStorage.setItem('listofbookmarks');
+  //listofbookmarks={place_name:name,zoom:zoom,center:coords};
+  //localStorage.setItem('listofbookmarks',JSON.stringify(listofbookmarks));
   localStorage.listofbookmarks.push(listofbookmarks);
   //localStorage.listofbookmarks=JSON.stringify(listofbookmarks);
   return false;
