16a549cdac071706465fb7eeb873fe7b61318ac6
Justin R. Miller <incanus@codesorcery.net>
2014-08-21 12:02:45 -0700

    fix missing require

--- index-shell.js
+++ index-shell.js
@@ -3,7 +3,7 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var BrowserWindow = require('browser-window');
 var Menu = require('menu');
-
+var shell = require('shell');
 
 var node = path.resolve(path.join(__dirname, 'vendor', 'node'));
 var script = path.resolve(path.join(__dirname, 'index-server.js'));
