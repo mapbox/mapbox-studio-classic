b85204453d14aa73624af09c98d2104b1853b4c5
samanpwbb <samanpwbb@gmail.com>
2014-08-27 13:06:17 -0400

    revert changes to tab switching

--- app/style.js
+++ app/style.js
@@ -307,7 +307,7 @@ Editor.prototype.keys = function(ev) {
     ev.preventDefault();
     window.editor.addBookmark(ev);
     break;
-  case ((which > 48 && which < 58)): // 1-9
+  case ((which > 48 && which < 58) && ev.altKey): // 1-9
     var tab = $('#tabs a.tab')[(which-48)-1];
     if (tab) $(tab).click();
     break;
