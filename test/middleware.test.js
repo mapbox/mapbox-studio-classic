5ee23343e084e06a1c0f3fb47f80c720f9a52b00
Young Hahn <young@mapbox.com>
2014-09-06 18:33:26 -0400

    Include more default styles.

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -82,7 +82,11 @@ test('examples: gets style info', function(t) {
             'mapbox-studio-default-style',
             'mapbox-studio-osm-bright',
             'mapbox-studio-mapbox-outdoors',
-            'mapbox-studio-satellite-afternoon'
+            'mapbox-studio-satellite-afternoon',
+            'mapbox-studio-pencil',
+            'mapbox-studio-comic',
+            'mapbox-studio-light',
+            'mapbox-studio-run-bike-and-hike'
         ], 'has example styles');
         t.equal(req.examples.style['mapbox-studio-default-style'].name, '');
         t.equal(req.examples.style['mapbox-studio-osm-bright'].name, 'OSM Bright 2');
