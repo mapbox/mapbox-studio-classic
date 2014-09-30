d3f29d57312ff23264d8c2d6fc724ebd095e2c6d
Young Hahn <young@mapbox.com>
2014-09-30 15:25:50 -0400

    Split out handlers for each ajax request for consistency test.

--- app/test/app.test.source.js
+++ app/test/app.test.source.js
@@ -271,26 +271,35 @@ tape('#raster and nonraster mix error', function(t) {
     t.equal($('#addlayer').size(), 1, 'shows #addlayer modal');
     $('#addlayer input[name=Datasource-file]').val(window.testParams.dataPath + '/shp/dc_bus_lines/DCGIS_BusLineLn.shp');
     $('#addlayer').submit();
-    onajax(function() {
+    onajax(afterOmnivore1);
+    onajax(afterUpdate1);
+    onajax(afterOmnivore2);
+    onajax(afterDelete);
+
+    function afterOmnivore1() { t.ok(!hasModal('#error'), 'no errors'); }
+
+    function afterUpdate1() {
         t.equal($('#layers-DCGIS_BusLineLn').size(), 1, 'adds #layers-DCGIS_BusLineLn form');
         $('#addlayer input[name=Datasource-file]').val(window.testParams.dataPath + '/geotiff/sample.tif');
         $('#addlayer').submit();
-        onajax(function() {
-            t.equal($('#layers-sample').size(), 0, 'no #layers-sample form');
+    }
 
-            t.ok(hasModal('#error'), 'shows error modal');
-            $('#error a.js-close').click();
-            t.ok(!hasModal('#error'), 'removes error modal');
+    function afterOmnivore2() {
+        t.equal($('#layers-sample').size(), 0, 'no #layers-sample form');
 
-            $('#del-DCGIS_BusLineLn').click();
-            t.ok(hasModal('#confirm'), 'shows confirm modal');
-            $('#confirm a.js-confirm').click();
-            onajax(function() {
-                t.equal($('#layers-DCGIS_BusLineLn').size(), 0, 'removes #layers-DCGIS_BusLineLn form');
-                t.end();
-            });
-        });
-    });
+        t.ok(hasModal('#error'), 'shows error modal');
+        $('#error a.js-close').click();
+        t.ok(!hasModal('#error'), 'removes error modal');
+
+        $('#del-DCGIS_BusLineLn').click();
+        t.ok(hasModal('#confirm'), 'shows confirm modal');
+        $('#confirm a.js-confirm').click();
+    }
+
+    function afterDelete() {
+        t.equal($('#layers-DCGIS_BusLineLn').size(), 0, 'removes #layers-DCGIS_BusLineLn form');
+        t.end();
+    }
 });
 
 var datatests = {
