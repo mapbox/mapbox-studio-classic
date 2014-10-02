246589ac6a6ab916d8fe6627f4031fc619ab562f
Young Hahn <young@mapbox.com>
2014-10-02 00:33:47 -0400

    Atom-specific loading modal for image exports. Refs #874.

--- app/atom.js
+++ app/atom.js
@@ -38,11 +38,15 @@ $(document).ready(function() {
                 filters: [{ name: typeExtension.toUpperCase(), extensions: [typeExtension]}]
             });
             if (filePath) {
+                window.Modal.show('atomexporting');
                 uri.method = 'GET';
                 var writeStream = fs.createWriteStream(filePath);
                 var req = http.request(uri, function(res) {
                     if (res.statusCode !== 200) return;
                     res.pipe(writeStream);
+                    writeStream.on('finish', function() {
+                        window.Modal.close();
+                    });
                 });
                 req.end();
             }
@@ -50,4 +54,11 @@ $(document).ready(function() {
         }
         // Passthrough everything else.
     });
+
+    window.Modal.options.templates.modalatomexporting = function() { return "\
+    <div id='atom-loading' class='modal round col6 margin3 pin-top top2 dark fill-dark'>\
+        <h3 class='center pad1y pad2x keyline-bottom'>Exporting</h3>\
+        <div class='row2 loading contain'></div>\
+    </div>";
+    };
 });
