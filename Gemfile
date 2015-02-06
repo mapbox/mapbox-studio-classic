c4027a0eae6f85360cf9ecabe0990b060670eb18
amyleewalton <amylee@mapbox.com>
2015-02-06 16:34:34 -0500

    adding Gemfile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'jekyll', '2.1.1'
+gem 'liquid', '2.6.1'
+gem 'rdiscount', '2.1.7'
--- jekyll-test.sh
+++ jekyll-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-bundle install --path .bundle
-bundle exec jekyll build --config=_config.test.yml --destination=_site-test
+gem install bundle
 
+bundle install --path .bundle
+bundle exec jekyll build
