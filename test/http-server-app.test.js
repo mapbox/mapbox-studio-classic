ff5ebd09b14cce5b91f0142ea676b4d03d8ffb94
Dane Springmeyer <dane@mapbox.com>
2015-03-05 14:01:18 -0800

    ensure style exists

--- test/http-server-app.test.js
+++ test/http-server-app.test.js
@@ -3,13 +3,16 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 var assert = require('assert');
 var http = require('http');
+var fs = require('fs');
 
 test('server', function(t) {
     var mbstudio = spawn(path.join(__dirname,'../index.js'), [], {});
     mbstudio.stdout.on('data', function (data) {
-      t.ok(data.toString().indexOf('Mapbox Studio @ http://localhost:3000') > -1);
+      t.ok(data.toString().indexOf('Mapbox Studio @ http://localhost:3000') > -1,'server started on port');
       var url = 'http://localhost:3000/style/14/8716/8015{format}?id=tmpstyle://'
-      url += path.resolve(path.join(__dirname,'../node_modules/mapbox-machine-styles/node_modules/mapbox-studio-streets'));
+      var style_path = path.resolve(path.join(__dirname,'../node_modules/mapbox-machine-styles/node_modules/mapbox-studio-streets'));
+      t.ok(fs.existsSync(style_path),'test style exists');
+      url += style_path;
       t.test('fetches @2x.png', function(st) {
           var png_url = url.replace('{format}','@2x.png');
           http.get(png_url,function(res) {
