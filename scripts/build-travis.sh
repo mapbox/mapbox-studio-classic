dcc718c889ea761fd1c4a2ae9895d26f7f22cea5
Dane Springmeyer <dane@mapbox.com>
2015-05-07 19:39:12 -0700

    ensure that publishing is only done for node v0.10.x

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -4,30 +4,35 @@ PLATFORM=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwx
 COMMIT_MESSAGE=$(git show -s --format=%B $1 | tr -d '\n')
 GITSHA=$(echo "$COMMIT_MESSAGE" | grep -oE '\[publish [a-z0-9\.\-]+\]' | grep -oE '[a-z0-9\.\-]+' | tail -n1)
 
-if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
-    set -eu
+if [[ ${PACKAGABLE:-false} == true ]]; then
 
-    echo "Publishing $GITSHA"
-    sudo apt-get update
-    sudo apt-get install -qqy curl unzip nsis python-pip mono-devel expect p7zip-full
-    sudo pip install -q awscli
-    sudo curl -Lsf https://github.com/mapbox/windowsign/archive/v0.0.1.tar.gz | \
-    sudo tar --strip 1 -xzf - --directory=/usr/local/bin "windowsign-0.0.1/windowsign"
-    ./scripts/build-atom.sh "$GITSHA" linux
-    ./scripts/build-atom.sh "$GITSHA" win32 x64
-    ./scripts/build-atom.sh "$GITSHA" win32 ia32
-    aws s3 ls s3://mapbox/mapbox-studio/ > listing.txt
-    ./scripts/generate-s3-listing.py < listing.txt > index.html
-    aws s3 cp --acl=public-read index.html s3://mapbox/mapbox-studio/index.html
-elif [ $PLATFORM == "darwin" ] && [ -n "$GITSHA" ]; then
-    echo "Publishing $GITSHA"
-    brew install python
-    brew link --overwrite python
+    if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
+        set -eu
 
-    set -eu
+        echo "Publishing $GITSHA"
+        sudo apt-get update
+        sudo apt-get install -qqy curl unzip nsis python-pip mono-devel expect p7zip-full
+        sudo pip install -q awscli
+        sudo curl -Lsf https://github.com/mapbox/windowsign/archive/v0.0.1.tar.gz | \
+        sudo tar --strip 1 -xzf - --directory=/usr/local/bin "windowsign-0.0.1/windowsign"
+        ./scripts/build-atom.sh "$GITSHA" linux
+        ./scripts/build-atom.sh "$GITSHA" win32 x64
+        ./scripts/build-atom.sh "$GITSHA" win32 ia32
+        aws s3 ls s3://mapbox/mapbox-studio/ > listing.txt
+        ./scripts/generate-s3-listing.py < listing.txt > index.html
+        aws s3 cp --acl=public-read index.html s3://mapbox/mapbox-studio/index.html
+    elif [ $PLATFORM == "darwin" ] && [ -n "$GITSHA" ]; then
+        echo "Publishing $GITSHA"
+        brew install python
+        brew link --overwrite python
 
-    pip install -q awscli
-    ./scripts/build-atom.sh "$GITSHA" darwin
+        set -eu
+
+        pip install -q awscli
+        ./scripts/build-atom.sh "$GITSHA" darwin
+    else
+        echo "Not publishing for $PLATFORM / $GITSHA / node $NODE_VERSION / atom $ATOM_VERSION"
+    fi
 else
-    echo "Not publishing for $PLATFORM / $GITSHA"
+    echo "Not marked packagable: $PLATFORM / $GITSHA / node $NODE_VERSION / atom $ATOM_VERSION"
 fi
