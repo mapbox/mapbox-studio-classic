f1aea8d7b71793edfd5197beaf398b5bc1ec3106
bergwerkgis <wb@bergwerk-gis.at>
2015-06-19 15:35:51 +0200

    new AppVeyor build scripts, take 4

--- scripts/build-appveyor.bat
+++ scripts/build-appveyor.bat
@@ -8,7 +8,7 @@ SET PATH=%HOME%;%PATH%
 IF NOT DEFINED SKIP_DL SET SKIP_DL=0
 IF %SKIP_DL% EQU 1 GOTO RUN_INSTALL
 
-REM find and remove default node.exe to avoid conflicts
+::delete default node.exe to avoid conflicts
 FOR /F "tokens=*" %%i in ('node -e "console.log(process.execPath)"') do SET NODE_EXE_PATH=%%i
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 ECHO deleting node.exe^: %NODE_EXE_PATH%
@@ -16,6 +16,7 @@ ECHO deleting node.exe^: %NODE_EXE_PATH%
 IF EXIST %NODE_EXE_PATH% DEL /Q %NODE_EXE_PATH%
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+::download custom mapbox node.exe
 SET ARCHPATH=
 if "%platform%"=="x64" SET ARCHPATH=x64/
 SET NODE_URL=https://mapbox.s3.amazonaws.com/node-cpp11/v%NODE_VERSION%/%ARCHPATH%node.exe
@@ -23,15 +24,19 @@ ECHO fetching %NODE_URL%
 powershell Invoke-WebRequest $env:NODE_URL -OutFile $env:HOME\node.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+::download vcredist DLLs
 SET VCREDIST_FILE=vcredist_%platform%-mini.7z
 SET VCREDIST_URL=https://mapbox.s3.amazonaws.com/windows-builds/visual-studio-runtimes/vcredist-VS2014-CTP4/%VCREDIST_FILE%
 ECHO fetching %VCREDIST_URL%
 IF NOT EXIST %HOME%\%VCREDIST_FILE% powershell Invoke-WebRequest $env:VCREDIST_URL -OutFile $env:HOME\$env:VCREDIST_FILE
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-7z -y e %VCREDIST_FILE%
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
 
 :RUN_INSTALL
+
+::extract vcredist next to node.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+7z -y e %VCREDIST_FILE% | %windir%\system32\FIND "ing archive"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 node -v
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
@@ -40,12 +45,17 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CALL npm -v
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-
 CALL npm install --fallback-to-build=false --toolset=v140
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-REM put dumpbin on path: required by check_shared_libs.py
-SET PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin;%PATH%
+::AFTER npm install: extract vcredist next to node-mapnik binaries
+ECHO ============ TODO do not hardcode path to binding dir =============
+SET BINDING_DIR=%HOME%\node_modules\mapnik\lib\binding\node-v11-win32-x64
+7z -y e %VCREDIST_FILE% -o%BINDING_DIR%\ | %windir%\system32\FIND "ing archive"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+::put dumpbin on path: required by check_shared_libs.py
+SET PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin;%PATH%
 python test\check_shared_libs.py .\
 ECHO ========== TODO ENABLE AGAIN ======== IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CALL npm test
