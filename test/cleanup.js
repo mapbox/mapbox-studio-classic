746394143af4401d0633486ca1f3a3d44e71240d
bergwerkgis <wb@bergwerk-gis.at>
2015-08-19 13:34:02 +0200

    hunting down timing problems

--- test/cleanup.js
+++ test/cleanup.js
@@ -1,7 +1,10 @@
 var rimraf = require('rimraf');
 var tm = require('../lib/tm');
 
-rimraf(tm.join(require('os').tmpdir(), 'mapbox-studio'), function(err) {
+var tmpdir = tm.join(require('os').tmpdir(), 'mapbox-studio');
+console.log('deleting temporary files:\n' + tmpdir);
+rimraf(tmpdir, function(err) {
     if (err) throw err;
+    console.log('temporary files deleted');
     process.exit(0);
 });
