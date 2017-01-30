e0bf42722b21145d8175070b79daead4dad67e6e
Dane Springmeyer <dane@mapbox.com>
2017-01-30 00:00:06 -0800

    try building on trusty

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -12,6 +12,8 @@ if [[ ${PACKAGABLE:-false} == true ]]; then
         echo "Publishing $GITSHA"
         sudo apt-get update
         sudo apt-get install -qqy curl unzip nsis python-pip mono-devel expect p7zip-full
+        nsis -v || true
+        nsis --version || true
         sudo pip install -q awscli
         sudo curl -Lsf https://github.com/mapbox/windowsign/archive/v0.0.1.tar.gz | \
         sudo tar --strip 1 -xzf - --directory=/usr/local/bin "windowsign-0.0.1/windowsign"
