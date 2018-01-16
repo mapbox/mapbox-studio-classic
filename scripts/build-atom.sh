129f29cba74d2be384ea0b7a1dc5455b20cd30db
Dane Springmeyer <dane@mapbox.com>
2018-01-16 15:19:22 -0800

    attemp to list keychains [publish test-publish]

--- scripts/build-atom.sh
+++ scripts/build-atom.sh
@@ -174,13 +174,17 @@ elif [ $platform == "darwin" ]; then
     aws s3 cp "s3://mapbox/mapbox-studio/keys2/Developer ID Certification Authority.cer" authority.cer
     aws s3 cp "s3://mapbox/mapbox-studio/keys2/Developer ID Application- Mapbox, Inc. (GJZR2MEM28).cer" signing-key.cer
     aws s3 cp "s3://mapbox/mapbox-studio/keys2/Mac Developer ID Application- Mapbox, Inc..p12" signing-key.p12
-    security create-keychain -p travis signing.keychain \
+    KEYCHAIN_NAME="signing.keychain"
+    security create-keychain -p travis ${KEYCHAIN_NAME} \
         && echo "+ signing keychain created"
-    security import authority.cer -k ~/Library/Keychains/signing.keychain -T /usr/bin/codesign \
+    security list-keychains -s ${KEYCHAIN_NAME}
+    security list-keychains
+    security show-keychain-info ${KEYCHAIN_NAME}
+    security import authority.cer -k ~/Library/Keychains/${KEYCHAIN_NAME} -T /usr/bin/codesign \
         && echo "+ authority cer added to keychain"
-    security import signing-key.cer -k ~/Library/Keychains/signing.keychain -T /usr/bin/codesign \
+    security import signing-key.cer -k ~/Library/Keychains/${KEYCHAIN_NAME} -T /usr/bin/codesign \
         && echo "+ signing cer added to keychain"
-    security import signing-key.p12 -k ~/Library/Keychains/signing.keychain -P "" -T /usr/bin/codesign \
+    security import signing-key.p12 -k ~/Library/Keychains/${KEYCHAIN_NAME} -P "" -T /usr/bin/codesign \
         && echo "+ signing key added to keychain"
     rm authority.cer
     rm signing-key.cer
@@ -190,10 +194,10 @@ elif [ $platform == "darwin" ]; then
     sudo ntpdate -u time.apple.com
 
     # Sign .app file.
-    codesign --keychain ~/Library/Keychains/signing.keychain --sign "Developer ID Application: Mapbox, Inc." --deep --verbose --force "$build_dir/Mapbox Studio.app"
+    codesign --keychain ~/Library/Keychains/${KEYCHAIN_NAME} --sign "Developer ID Application: Mapbox, Inc." --deep --verbose --force "$build_dir/Mapbox Studio.app"
 
     # Nuke signin keychain.
-    security delete-keychain signing.keychain
+    security delete-keychain ${KEYCHAIN_NAME}
 
     # Use ditto rather than zip to preserve code signing.
     ditto -c -k --sequesterRsrc --keepParent --zlibCompressionLevel 9 $(basename $build_dir) $build_dir.zip
